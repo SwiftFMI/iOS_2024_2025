ff2ce22aca7199b8f4b2e9998ae15356de7c63e2
Emil Atanasov <heitara@gmail.com>
2024-11-29 18:16:44 +0200

    feat: animation example

---
+++ projects/AnimationApp/AnimationApp/CompactView.swift
@@ -0,0 +1,18 @@
+//
+//  CompactView.swift
+//  AnimationApp
+//
+//  Created by Emil Atanasov on 11/18/24.
+//
+
+import SwiftUI
+
+struct CompactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompactView()
+}
---
+++ projects/AnimationApp/AnimationApp/ExpandedView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpandedView.swift
+//  AnimationApp
+//
+//  Created by Emil Atanasov on 11/18/24.
+//
+
+import SwiftUI
+
+struct ExpandedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpandedView()
+}
